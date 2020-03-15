e26034da56416cea56a340dd2f9e4690f1ec6a05
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-03-16 00:11:49 +0300

    add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './spinner.css'
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-pulse-fogvyxw5uej">
+            <div className="ldio-3qgi4mlcq03">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
+    
